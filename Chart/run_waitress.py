2b1ab08b6e76a74b78122e4171d4516cb775a163
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-05-21 01:12:46 +0530

    Update run_waitress.py

--- Chart/run_waitress.py
+++ Chart/run_waitress.py
@@ -1,5 +1,7 @@
+import os
 from waitress import serve
 from Chart.wsgi import application
 
 if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Chart.settings')
     serve(application, host='0.0.0.0', port=8000)
