6b9f9a79e5f3c44a93dfa28b7240c6a775a090aa
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-05-21 05:21:09 +0530

    Update settings.py

--- Chart/Chart/settings.py
+++ Chart/Chart/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4(%)fh0@1&t_=j^necffvi-%dqpwpb4a385#r=am-vgh&rylau
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://global-and-regional-trend-data-with-n1b5.onrender.com']
+ALLOWED_HOSTS = ['global-and-regional-trend-data-with-n1b5.onrender.com']
 
 
 # Application definition
