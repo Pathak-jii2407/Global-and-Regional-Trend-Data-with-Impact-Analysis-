50f1cc518f2839d036c5b7e41dba99781c612136
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-05-21 04:56:24 +0530

    Update urls.py

--- Chart/base/urls.py
+++ Chart/base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('get_data/', views.get_data, name='get_data'),
+    path('get_data/brief-detail/<int:detail_id>/', views.brief_detail_view, name='brief_detail'),
     path('get_pie_chart/', views.get_pie_chart, name='get_pie_chart'),
     path('pie_chart/', views.pie_chart, name='pie_chart'),
     path('get_intensity_by_sector/', views.get_intensity_by_sector, name='get_intensity_by_sector'),
@@ -16,6 +17,7 @@ urlpatterns = [
     path('impact_by_region/', views.impact_by_region, name='impact_by_region'),
     path('get_trends_over_years/', views.get_trends_over_years, name='get_trends_over_years'),
     path('trends_over_years/', views.trends_over_years, name='trends_over_years'),
+    path('subscribe', views.subscribe, name='subscribe'),
 
 
 ]
